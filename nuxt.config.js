c19797bb9a6687cdb7c6734a502e905b9b20a092
Miftahul Hidayati <miftahulhdyt@gmail.com>
2022-01-07 10:45:58 +0700

    update backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://miftahstartup.herokuapp.com',
   },
   auth: {
     strategies: {
